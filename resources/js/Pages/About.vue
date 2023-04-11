165d7c6fea1c05ea1798cdb4f5c450652f7f188a
mrfneto <mrfneto@gmail.com>
2023-04-11 18:05:08 -0300

    Add Inertia js

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+defineProps({
+    about_us: String,
+});
+</script>
+<template>
+    <div class="">
+        <h1>Inertiajs is {{ about_us }}</h1>
+
+        <Link href="/">Go to Home</Link>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+defineProps({
+    test: String,
+});
+</script>
+<template>
+    <div class="">
+        <h1>Inertiajs is {{ test }}</h1>
+
+        <Link href="/about-us">Go to about us</Link>
+    </div>
+</template>
